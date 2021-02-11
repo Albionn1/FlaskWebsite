b017c76c76b20f1be46a8532ad27e05afac9ecca
Albionn1 <57451316+Albionn1@users.noreply.github.com>
2021-02-11 01:49:58 +0100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet,e tjera',
+            'Author':'Albion Berisha',
+            'create_date':'04-02-2021'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'Lorem ipsum dolor sit amet,e tjera',
+            'Author':'Albin Berisha',
+            'create_date':'04-02-2021'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'Lorem ipsum dolor sit amet,e tjera',
+            'Author':'Albion Berisha',
+            'create_date':'04-02-2021'
+        },
+    ]
+    return articles
